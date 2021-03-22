056ce3cd9f8aea41b46848fa05a5e72bb6c8929a
Maxim Kuziev <kuziev@burogs.com>
2021-03-22 23:05:56 +0300

    new Provider

---
+++ 3rdparty/hybridauth/hybridauth/src/Provider/Unomas.php
@@ -0,0 +1,103 @@
+<?php
+/*!
+* Hybridauth
+* https://hybridauth.github.io | https://github.com/hybridauth/hybridauth
+*  (c) 2017 Hybridauth authors | https://hybridauth.github.io/license.html
+*/
+
+namespace Hybridauth\Provider;
+
+use Hybridauth\Adapter\OAuth2;
+use Hybridauth\Exception\Exception;
+use Hybridauth\Exception\UnexpectedApiResponseException;
+use Hybridauth\Data\Collection;
+use Hybridauth\User\Profile;
+
+/**
+ * Unomas provider adapter.
+ *
+ * Example:
+ *
+ *   $config = [
+ *       'callback'  => Hybridauth\HttpClient\Util::getCurrentUrl(),
+ *       'keys'      => ['id' => '', 'secret' => ''],
+ *   ];
+ *
+ *   $adapter = new Hybridauth\Provider\Unomas($config);
+ *
+ *   try {
+ *       if (!$adapter->isConnected()) {
+ *           $adapter->authenticate();
+ *       }
+ *
+ *       $userProfile = $adapter->getUserProfile();
+ *   }
+ *   catch(\Exception $e) {
+ *       print $e->getMessage() ;
+ *   }
+ */
+class Unomas extends OAuth2
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $apiBaseUrl = 'https://login.yandex.ru/info';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $authorizeUrl = 'https://oauth.yandex.ru/authorize';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $accessTokenUrl = 'https://oauth.yandex.ru/token';
+
+    /**
+     * load the user profile from the IDp api client
+     *
+     * @throws Exception
+     */
+    public function getUserProfile()
+    {
+
+        $this->scope = implode(',', []);
+
+        $response = $this->apiRequest($this->apiBaseUrl . "?format=json");
+
+        if (!isset($response->id)) {
+            throw new UnexpectedApiResponseException('Provider API returned an unexpected response.');
+        }
+
+        $data = new Collection($response);
+
+        if (!$data->exists('id')) {
+            throw new UnexpectedApiResponseException('Provider API returned an unexpected response.');
+        }
+
+        $userProfile = new Profile();
+
+        $userProfile->identifier = $data->get('id');
+        $userProfile->firstName = $data->get('real_name');
+        $userProfile->lastName = $data->get('family_name');
+        $userProfile->displayName = $data->get('display_name');
+        $userProfile->photoURL = 'http://upics.yandex.net/' . $userProfile->identifier . '/normal';
+        $userProfile->profileURL = "";
+        $userProfile->gender = $data->get('sex');
+        $userProfile->email = $data->get('default_email');
+        $userProfile->emailVerified = $data->get('default_email');
+
+        if( strpos($userProfile->email,'unomas-mebel.ru')==false) {
+            throw new UnexpectedApiResponseException('Domain not allowed');
+        }
+
+        if ($data->get('birthday')) {
+            list($birthday_year, $birthday_month, $birthday_day) = explode('-', $response->birthday);
+            $userProfile->birthDay = (int)$birthday_day;
+            $userProfile->birthMonth = (int)$birthday_month;
+            $userProfile->birthYear = (int)$birthday_year;
+        }
+
+        return $userProfile;
+    }
+}
